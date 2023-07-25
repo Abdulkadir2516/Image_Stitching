c5dcc89cab54d27e5443728fec47f9dc7d0a23e7
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-07-25 18:42:16 +0300

    BRIEF Algoritması ve diğer güncellemeler

--- SURF.py
+++ SURF.py
@@ -5,6 +5,7 @@ img = cv2.imread('data/ornek.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 surf = cv2.SURF_create()
+# OpenCV'nin 4.0.0 sürümünden itibaren, SURF algoritması OpenCV'nin ana dağıtımından çıkarılmıştır.
 
 keypoints, descriptors = surf.detectAndCompute(img, None)
 
